6720c0b194b8b8ec857472ee394c0fe06378b587
unknown <kdsuraj786@gmail.com>
2025-12-01 12:24:24 +0530

    favico and gtags

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
       <h1>Convert JPG to PDF — Fast & Private</h1>
       <p className="small">Client-side conversion. Your files never leave your browser by default.</p>
       <div style={{marginTop:20}}>
-        <Link href="/tools/jpg-to-pdf"><button className="btn">Open JPG → PDF Converter</button></Link>
+        <Link href="/tools/jpg-to-pdf"><button className="btn" style={{
+                    background: "#0b74de",
+                    color: "#fff",
+                    padding: "10px 16px",
+                    borderRadius: "8px",
+                }}>Open JPG → PDF Converter</button></Link>
       </div>
     </section>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,15 @@ export default function Header() {
     return (
         <header className="container header" style={{ paddingTop: 12, paddingBottom: 12 }}>
             <div>
-                <Link href="/" style={{fontSize:'24px', color: '#0b74de!important', textDecoration: 'none' }}><strong>JPG→PDF</strong></Link>
+                <Link href="/" style={{fontSize:'24px', color: '#0b74de', textDecoration: 'none' }}><strong>JPG→PDF</strong></Link>
             </div>
             <nav>
-                <Link href="/tools/jpg-to-pdf" style={{ marginRight: 12, color: '#0b74de!important', textDecoration: 'none' }}>Tool</Link>
-                <Link href="/blog" style={{ marginRight: 12, color: '#0b74de!important', textDecoration: 'none' }}>Blog</Link>
-                <Link href="/privacy" style={{ color: '#0b74de!important', textDecoration: 'none' }}>Privacy</Link>
+                <Link href="/tools/jpg-to-pdf" 
+                style={{ marginRight: 12, color: '#0b74de', textDecoration: 'none' }}>Tool</Link>
+                <Link href="/blog" 
+                style={{ marginRight: 12, color: '#0b74de', textDecoration: 'none' }}>Blog</Link>
+                <Link href="/privacy" 
+                style={{ color: '#0b74de', textDecoration: 'none' }}>Privacy</Link>
             </nav>
         </header>
     )
